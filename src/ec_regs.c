9143956b1b190be06bc5cbaeccc99ae9121e9026
raz <raziebe@gmail.com>
2012-10-29 22:25:59 +0200

    added 910

--- src/ec_regs.c
+++ src/ec_regs.c
@@ -2,8 +2,15 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <net/ethernet.h>
+#include <time.h>
 
 #include "ethercattype.h"
+
+#define NSEC_PER_SEC (1000000000L)
+#define FREQUENCY 1000
+#define CLOCK_TO_USE CLOCK_REALTIME
+#define PERIOD_NS (NSEC_PER_SEC / FREQUENCY)
+#define TIMESPEC2NS(T) ((uint64_t) (T).tv_sec * NSEC_PER_SEC + (T).tv_nsec)
 
 static uint8_t ec_registers[ECT_REG_DCCYCLE1] = { 0 };
 
@@ -15,8 +22,8 @@ void ec_init_regs(void)
 	ec_registers[ECT_BASE_REVISION] = 0x11;	/* base revision. 1byte */
 	ec_registers[ECT_BASE_BUILD1] = 0x01;	/* base build .2bytes   */
 	ec_registers[ECT_BASE_BUILD2] = 0x22;
-	ec_registers[ECT_BASE_FMMUS] = 0x1;	/* base fmmu count 1byte */
-	ec_registers[ECT_BASE_SYNCM] = 0x4;	/* base sync count 1byte */
+	ec_registers[ECT_BASE_FMMUS] = 0x0;	/* base fmmu count 1byte */
+	ec_registers[ECT_BASE_SYNCM] = 0x0;	/* base sync count 1byte */
 	ec_registers[ECT_REG_PORTDES] = 0x000F;	/* octet 1      0x000F one port. mii */
 	ec_registers[ECT_REG_ESCSUP] = 0b00000001 | 0b00000100;
 	/* octet 2 0b00000001   fmmu bit operation
@@ -35,14 +42,15 @@ int16_t ec_station_address(void)
 
 int ec_raw_set_ado(int reg, uint8_t * data, int datalen)
 {
-	int ret = 1;
-
 	if (reg > ECT_REG_DCCYCLE1 || reg < ECT_REG_TYPE) {
 		printf("%s insane ado\n",__FUNCTION__);
 		return 0;
 	}
 	memcpy(&ec_registers[reg], data, datalen);
-	return ret;
+	if (reg == ECT_REG_ALCTL){
+		memcpy(&ec_registers[ECT_REG_ALSTAT], data, datalen);
+	}
+	return 0;
 }
 
 void ec_raw_get_ado(int reg, uint8_t * data, int datalen)
@@ -51,5 +59,18 @@ void ec_raw_get_ado(int reg, uint8_t * data, int datalen)
 		printf("%s insane ado\n",__FUNCTION__);
 		return;
 	}
+	if (reg == ECT_REG_DCSYSTIME){
+
+		struct timespec tm;
+	    uint64_t t;
+	    uint8_t *p = (uint8_t *)&t;
+
+	    clock_gettime(CLOCK_TO_USE, &tm);
+	    t = TIMESPEC2NS(tm);
+	    if (datalen == 4)
+	    	p += 4;
+	    memcpy(data, p, datalen);
+	    return;
+	  }
 	memcpy(data, &ec_registers[reg], datalen);
 }
