d3ac1b63342daf4761d0058f12df9cd527fe391a
raz <raziebe@gmail.com>
2012-11-09 00:30:30 +0200

    alala

--- src/ec_cmd_lrd.c
+++ src/ec_cmd_lrd.c
@@ -4,23 +4,18 @@
 #include "fsm_slave.h"
 #include "ecs_slave.h"
 #include "ec_regs.h"
-#include <arpa/inet.h>
+#include "ec_process_data.h"
 
+uint8_t x = 0;
  /** Logical Read Write */
 void ec_cmd_lrd(e_slave *ecs, uint8_t *dgram_ec)
 {
-/*
-	int val = 0;
-	uint16_t datalen = ec_dgram_data_length(slave->pkt);
-	uint8_t *datagram = (uint8_t *) __ecat_frameheader(slave->pkt);
-	uint16_t size = ec_dgram_size(slave->pkt);
-	uint8_t *data = ec_dgram_data(slave->pkt);
-	uint32_t offset = 0;
-*/
-	__ec_inc_wkc__(dgram_ec);
-//	printf("%s index=%d\n",
-//	       __FUNCTION__, ecs->pkt_index);
+	uint16_t datalen = __ec_dgram_dlength(dgram_ec);
+	uint8_t *data = __ec_dgram_data(dgram_ec);
+	uint32_t offset = __ec_dgram_laddr(dgram_ec);
+	
+	get_process_data(data, offset,datalen);
 
-//LWR_OUT:
+	__ec_inc_wkc__(dgram_ec);
   	__set_fsm_state(ecs, ecs_process_next_dgram);
 }
