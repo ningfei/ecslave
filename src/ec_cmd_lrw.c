7d8709a97ce6b7d12e13bf69504c9f4793b29526
raz <raziebe@gmail.com>
2012-11-09 00:30:40 +0200

    modified:   src/ec_cmd_lrw.c

--- src/ec_cmd_lrw.c
+++ src/ec_cmd_lrw.c
@@ -4,33 +4,17 @@
 #include "fsm_slave.h"
 #include "ecs_slave.h"
 #include "ec_regs.h"
-#include <arpa/inet.h>
+#include "ec_process_data.h"
 
-uint8_t process_data[65536];
-
-void set_data_lrw(uint8_t * data, uint16_t offset, uint16_t datalen)
-{
-	memcpy(&process_data[offset], data, datalen);
-}
-
-void get_data_lrw(uint8_t * data, uint16_t offset, uint16_t datalen)
-{
-	memcpy(data, &process_data[offset], datalen);
-}
-
- /** Logical Read Write */
+/** Logical Read Write */
 void ec_cmd_lrw(e_slave * ecs,uint8_t *dgram_ec)
 {
-/*
-	int val = 0;
-	uint16_t datalen = ec_dgram_data_length(slave->pkt);
-	uint8_t *datagram = (uint8_t *) __ecat_frameheader(slave->pkt);
-	uint16_t size = ec_dgram_size(slave->pkt);
-	uint8_t *data = ec_dgram_data(slave->pkt);
-	uint32_t offset = 0;
-*/
+	uint16_t datalen = __ec_dgram_dlength(dgram_ec);
+	uint8_t *data = __ec_dgram_data(dgram_ec);
+	uint32_t offset = __ec_dgram_laddr(dgram_ec);
+	
+	set_process_data(data, offset,datalen);
 	__ec_inc_wkc__(dgram_ec);
 
-//LWR_OUT:
      __set_fsm_state(ecs, ecs_process_next_dgram);
 }
