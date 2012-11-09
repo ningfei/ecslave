96868864eee38672f39e9228019de8e1bd1879e0
raz <raziebe@gmail.com>
2012-11-10 01:43:16 +0200

    process data errors

--- src/ec_process_data.c
+++ src/ec_process_data.c
@@ -3,7 +3,19 @@
 #include "ethercattype.h"
 #include "ec_process_data.h"
 
+uint8_t process_data[66500];
+
 void init_process_data(void)
 {
-	memset(process_data,'x',sizeof(process_data));
+	memset(process_data, 'x', sizeof(process_data));
+}
+
+void set_process_data(uint8_t *data, uint16_t offset, uint16_t datalen)
+{
+	memcpy(&process_data[offset], data, datalen);
+}
+
+void get_process_data(uint8_t * data, uint16_t offset, uint16_t datalen)
+{
+	memcpy(data, &process_data[offset], datalen);
 }
--- src/ec_process_data.h
+++ src/ec_process_data.h
@@ -1,18 +1,9 @@
 #ifndef  __EC_PROCESS_DATA_H__
 #define  __EC_PROCESS_DATA_H__
 
-static uint8_t process_data[65536];
-
-static inline void set_process_data(uint8_t * data, uint16_t offset, uint16_t datalen)
-{
-	memcpy(&process_data[offset], data, datalen);
-}
-
-static inline void get_process_data(uint8_t * data, uint16_t offset, uint16_t datalen)
-{
-	memcpy(data, &process_data[offset], datalen);
-}
-
+extern uint8_t process_data[];
+void set_process_data(uint8_t * data, uint16_t offset, uint16_t datalen);
+void get_process_data(uint8_t * data, uint16_t offset, uint16_t datalen);
 void init_process_data(void);
 
 #endif
