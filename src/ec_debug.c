27432808c46ab9be5687360e42a176865529124c
raz <raziebe@gmail.com>
2012-11-17 00:06:43 +0200

    moved dump buffer to debug

--- src/ec_debug.c
+++ src/ec_debug.c
@@ -1,2 +1,14 @@
+#include <stdio.h>
+#include "ethercattype.h"
+
+void ec_dump_string(uint8_t *c,int len)
+{
+	int i ;
+
+	for  (i = 0 ; i < len ; i++){
+		printf("%02X ", c[i]);
+	}
+	puts("");
+}
 
 void ec_printf(const char *str, ...){}
