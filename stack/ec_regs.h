3f9818befd920facd3f8728f128c1869485b7ccd
raz <raziebe@gmail.com>
2012-12-01 22:17:04 +0200

    aaa

--- stack/ec_regs.h
+++ stack/ec_regs.h
@@ -9,8 +9,11 @@
 #define EC_REGS_H_
 
 void ec_init_regs(e_slave *);
-void ec_set_ado(e_slave *,int reg, uint8_t * data, int datalen);
-void ec_get_ado(e_slave*,int reg, uint8_t * data, int datalen);
+void ec_set_ado(int reg, long val);
+void ec_get_ado(int reg, uint8_t * data);
+void ec_raw_set_ado(e_slave *,int reg, uint8_t * data, int datalen);
+void ec_raw_get_ado(e_slave*,int reg, uint8_t * data, int datalen);
+uint32_t ecat_cyclic_interval_ns(void);
 
 long sdo_high(void);
 int16_t ec_station_address(void);
